c13aa0468487731ea5ea8a2c6205a97bab4a0bcb
Milan Patel <61632091+milanLFC@users.noreply.github.com>
2022-02-04 12:24:28 +0000

    Update M_logging_example.py

--- M_logging_example.py
+++ M_logging_example.py
@@ -1,54 +1,67 @@
-import logging
-import os
-import time
 from datetime import datetime
+from logging.handlers import RotatingFileHandler
 
 # constants
 # select one of these two
-directory = '../some/directory/somewhere'
-directory = '.'
+logDirectory = '../some/directory/somewhere'
+logDirectory = '.'
 
-# set up some housekeeping
-if directory == '.':
-    '''
-        Get Current working Directory
-    '''
-    currentDirectory = os.getcwd()
-    logfilename = currentDirectory + "/" + os.path.basename(__file__).replace('.py','.log')
-else:
-    logfilename = directory + os.path.basename(__file__).replace('.py','.log')
 
-print("log file name is " + logfilename)
 
-logging.basicConfig(
-    filename=logfilename,
-    filemode='a',
-    format='%(asctime)s : %(filename)s : %(levelname)s : %(message)s', 
-    level=logging.DEBUG
-) 
 
-'''
-logging.debug('This is a debug message')
-logging.info('This is an info message')
-logging.warning('This is a warning message')
-logging.error('This is an error message')
-logging.critical('This is a critical message')
-'''
+def setUpLogging(directory, scriptName=0, DEBUG=0):
 
-# some example log entries
-logging.info('>>> BEGIN:  %s' % os.path.basename(__file__))
-totalConversionStartTime = datetime.now()
-logging.info('>>> START TIME: %s' % totalConversionStartTime)
+  # set up some housekeeping
+  if directory == '.':
+      '''
+          Get Current working Directory
+      '''
+      currentDirectory = os.getcwd()
+      if scriptName == 0:
+        logfilename = currentDirectory + "/" + os.path.basename(__file__).replace('.py','.log')
+      else:
+        logfilename = currentDirectory + "/" + scriptName.replace('.py','.log')
+  else:
+    if scriptName == 0:
+      logfilename = directory + os.path.basename(__file__).replace('.py','.log')
+    else:
+      logfilename = directory + "/" + scriptName.replace('.py','.log')
 
-logging.info('Working on directory: ' + currentDirectory)
+  if DEBUG:
+    print("log file name is " + logfilename)
 
-logging.debug('>>> Starting loop looking for files in directory')
+  logging.basicConfig(
+      handlers = [RotatingFileHandler(logfilename, maxBytes=1000000, backupCount=5)],
+      format='%(asctime)s : %(filename)s : %(levelname)s : %(message)s', 
+      level=logging.DEBUG
+  ) 
 
-logging.info('====================================================================')
 
-logging.info('Working on file : ' + 'some filename or variable')
+  return logging
 
-newFilename = 'bla.py'
-logging.debug("New filename is " + newFilename)
+if __name__ == '__main__':
 
-logging.info('>>> COMPLETED:  %s' % os.path.basename(__file__))
+  setUpLogging(logDirectory)
+
+  '''
+  logging.debug('This is a debug message')
+  logging.info('This is an info message')
+  logging.warning('This is a warning message')
+  logging.error('This is an error message')
+  logging.critical('This is a critical message')
+  '''
+  logging.info('>>> BEGIN:  %s' % os.path.basename(__file__))
+  totalConversionStartTime = datetime.now()
+  logging.info('>>> START TIME: %s' % totalConversionStartTime)
+
+  logging.debug('>>> Starting loop looking for files in directory')
+
+  logging.info('====================================================================')
+
+  logging.info('Working on file : ' + 'some filename or variable')
+
+  newFilename = 'bla.py'
+  logging.debug("New filename is " + newFilename)
+
+  logging.info('>>> COMPLETED:  %s' % os.path.basename(__file__))
+
